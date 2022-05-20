5fd9f2ac3ace99f0c51a1193bbbad9b920452760
Eduardo Medrano <eduardomedranop@gmail.com>
2022-05-20 13:08:09 -0400

    Clase3 completa

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,7 +3,7 @@ import './ItemListContainer.css';
 const ItemListContainer = () => {
   return (
     <div className="container mx-auto">
-      <div className="ItemListContainer">Aqui Va mi Contenido</div>
+      <div className="ItemListContainer">Aqui Va mi Contenido!!!!!</div>
     </div>
   );
 }
