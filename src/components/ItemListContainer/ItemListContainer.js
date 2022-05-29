842ebc63689ee93a9393bf97399edfefde2e280d
Eduardo Medrano <eduardomedranop@gmail.com>
2022-05-29 17:49:51 -0400

    clase5 v1

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,48 +1,12 @@
 import "./ItemListContainer.css";
-import ItemCount from "./ItemCount"
+import ItemList  from "../Items/ItemList";
 
 const ItemListContainer = () => {
-  // Create a new ItemListContainer
-
+  
   return (
     <>
-      <div className="flex gap-4 ">
-        {/* Card de Producto */}
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src="https://api.lorem.space/image/pizza?w=400&h=225" alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Producto</h2>
-            <p className="text-left">Descripción del Producto</p>
-            <div class="flex w-100">
-              <form className="text-left w-3/5" method="post" action>
-                <div className="tamano-contenedor">
-                  <input class="radio radio-primary" type="radio" id="personal" name="tamano"  checked/> Personal
-                </div>
-                <div className="tamano-contenedor">
-                  <input class="radio radio-primary" type="radio" id="mediana" name="tamano" /> Mediana
-                </div>
-                <div className="tamano-contenedor">
-                  <input className="radio radio-primary" type="radio" id="familiar" name="tamano" /> Familiar
-                </div>
-                <div className="tamano-contenedor">
-                  <input className="radio radio-primary" type="radio" id="extra-grande" name="tamano" /> Extra Grande
-                </div>
-              </form>
-              <div className="precio text-right w-2/5">
-                <span>Desde $7.000</span>
-              </div>
-            </div>
-            {/* Contador */}
-            <ItemCount stock={5} initial={1} />
-            {/* END Contador */}
-            <div className=" text-neutral font-bold mt-4">
-              <a href="google.com">Ver Mas Información</a>
-            </div>
-          </div>
-        </div>
-        {/* END Card de Producto */}
+      <div className="grid lg:grid-cols-4  md:grid-cols-2 gap-3 sm:grid-cols-2">
+        <ItemList></ItemList>
       </div>
     </>
   );
---
+++ src/utils/productos-mock.js
@@ -0,0 +1,186 @@
+const productos = [
+  {
+    id: 1,
+    titulo: "Peperoni",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-peperoni.jpg",
+    precio_final: 0,
+    stock: 25,
+    tamano: [
+      {
+        name: "Personal",
+        price: 7000,
+      },
+      {
+        name: "Mediana",
+        price: 12000,
+      },
+      {
+        name: "Familiar",
+        price: 16000,
+      },
+      {
+        name: "Extra Grande",
+        price: 25000,
+      },
+    ],
+  },
+  {
+    id: 2,
+    titulo: "Diabla",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-diabla.jpg",
+    precio_final: 0,
+    stock: 14,
+    tamano: [
+      {
+        name: "Personal",
+        price: 6500,
+      },
+      {
+        name: "Mediana",
+        price: 12000,
+      },
+      {
+        name: "Familiar",
+        price: 24000,
+      },
+      {
+        name: "Extra Grande",
+        price: 32000,
+      },
+    ],
+  },
+  {
+    id: 3,
+    titulo: "Grecia",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-grecia.jpg",
+    precio_final: 0,
+    stock: 12,
+    tamano: [
+      {
+        name: "Personal",
+        price: 4000,
+      },
+      {
+        name: "Mediana",
+        price: 7000,
+      },
+      {
+        name: "Familiar",
+        price: 14000,
+      },
+      {
+        name: "Extra Grande",
+        price: 28000,
+      },
+    ],
+  },
+  {
+    id: 4,
+    titulo: "Peter",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-peter.jpg",
+    precio_final: 0,
+    stock: 5,
+    tamano: [
+      {
+        name: "Personal",
+        price: 7000,
+      },
+      {
+        name: "Mediana",
+        price: 12000,
+      },
+      {
+        name: "Familiar",
+        price: 16000,
+      },
+      {
+        name: "Extra Grande",
+        price: 25000,
+      },
+    ],
+  },
+  {
+    id: 5,
+    titulo: "Pizza TOP",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-pizzatop.jpg",
+    precio_final: 0,
+    stock: 25,
+    tamano: [
+      {
+        name: "Personal",
+        price: 8000,
+      },
+      {
+        name: "Mediana",
+        price: 14000,
+      },
+      {
+        name: "Familiar",
+        price: 28000,
+      },
+      {
+        name: "Extra Grande",
+        price: 38000,
+      },
+    ],
+  },
+  {
+    id: 6,
+    titulo: "Romana",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-romana.jpg",
+    precio_final: 0,
+    stock: 25,
+    tamano: [
+      {
+        name: "Personal",
+        price: 6000,
+      },
+      {
+        name: "Mediana",
+        price: 10000,
+      },
+      {
+        name: "Familiar",
+        price: 22000,
+      },
+      {
+        name: "Extra Grande",
+        price: 32000,
+      },
+    ],
+  },
+  {
+    id: 6,
+    titulo: "Toscana",
+    descripcion: "Descripción del Producto",
+    imagen: "/img-pizzas/p-toscana.jpg",
+    precio_final: 0,
+    stock: 25,
+    tamano: [
+      {
+        name: "Personal",
+        price: 8000,
+      },
+      {
+        name: "Mediana",
+        price: 10000,
+      },
+      {
+        name: "Familiar",
+        price: 22000,
+      },
+      {
+        name: "Extra Grande",
+        price: 32000,
+      },
+    ],
+  },
+];
+
+export default productos;
