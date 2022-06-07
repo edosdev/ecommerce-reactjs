482c09f5db02e1c65ecd589468005328931cefd3
Eduardo Medrano <eduardomedranop@gmail.com>
2022-06-07 18:27:46 -0400

    clase7v1.2.3

--- src/components/Items/ItemList.js
+++ src/components/Items/ItemList.js
@@ -5,6 +5,8 @@ import productos from "../../utils/productos-mock";
 const ItemList = ({ filtrocat }) => {
   console.log("ejecutando filtro", filtrocat);
   const [products, setProducts] = useState([]);
+  const [filtro, setCategoria] = useState([]);
+ 
   const getProducts = () => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -21,9 +23,11 @@ const ItemList = ({ filtrocat }) => {
   useEffect(() => {
     getProducts()
       .then((response) => {
-
+        if (filtrocat) {
+          setCategoria(response);
+        } else {
           setProducts(response);
-
+        }
       })
 
       .catch((err) => {
@@ -35,11 +39,32 @@ const ItemList = ({ filtrocat }) => {
       });
   }, [filtrocat]);
 
-
+  
+    const resultadosb = filtro.filter((e) => e.categoria == filtrocat );
+    
+    console.log(resultadosb);
 
   return (
-    
     <>
+
+     {resultadosb.map((resultado) => {
+        return (
+          <Item
+            id={resultado.id}
+            titulo={resultado.titulo}
+            descripcion={resultado.descripcion}
+            imagen={resultado.imagen}
+            precio_final={resultado.precio_final}
+            stock={resultado.stock}
+            tamano={resultado.tamano.map((t) => (
+              <div className="tamano-contenedor">
+                <input class="radio radio-primary" type="radio" id={t.name} name="tamano" value={t.price} /> {t.name}
+              </div>
+            ))}
+          ></Item>
+        );
+     })}
+
       {products.map((producto) => {
         // console.log("este es el producto: ", producto);
         return (
