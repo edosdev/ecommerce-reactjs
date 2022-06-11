b01ddef1c5aace25919ecabe20b777dd03403f45
Eduardo Medrano <eduardomedranop@gmail.com>
2022-06-11 13:19:15 -0400

    Add new Readme & config firebase

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASclR0fFXxn9x0qHlpDwOK2naD0DFxqsk",
+  authDomain: "pizza-top-442cd.firebaseapp.com",
+  projectId: "pizza-top-442cd",
+  storageBucket: "pizza-top-442cd.appspot.com",
+  messagingSenderId: "412667016423",
+  appId: "1:412667016423:web:e1d920685b57df9c6dde42",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
