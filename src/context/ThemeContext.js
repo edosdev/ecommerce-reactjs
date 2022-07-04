cfb3200929ae2b09abf6d948d5dab8a6714e38dc
Eduardo Medrano <eduardomedranop@gmail.com>
2022-07-04 16:30:57 -0400

    0407221631

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,16 +3,13 @@ import { createContext, useState } from "react";
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
+  // eslint-disable-next-line
   const [temaOscuro, setTemaOcuro] = useState(false);
   const data = {
     temaOscuro,
   };
 
-  return (
-    <ThemeContext.Provider value={data}>
-    {children}
-    </ThemeContext.Provider>
-  )
+  return <ThemeContext.Provider value={data}>{children}</ThemeContext.Provider>;
 };
 
 export { ThemeContext };
